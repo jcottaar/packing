f261b2e57369a93044ced3b377bde29379d8220b
Jeroen Cottaar <199474359+jcottaar@users.noreply.github.com>
2025-12-18 06:22:10 +0000

    Update settings

--- core/pack_runner.py
+++ core/pack_runner.py
@@ -202,35 +202,35 @@ def baseline_runner(fast_mode=False):
     res = Runner()
     res.label = 'Baseline'
     
-    res.modifier_dict['scale_population'] = pm(1., lambda r:r.uniform(4.,7.), scale_population_size)
+    res.modifier_dict['scale_population'] = pm(1., lambda r:4., scale_population_size)
     res.modifier_dict['genetic_diversity'] = pm(True, lambda r:r.choice([True]), set_genetic_diversity)
     res.modifier_dict['alt_diversity'] = pm(False, lambda r:r.choice([False]), set_alt_diversity)
-    res.modifier_dict['no_jiggle'] = pm(False, lambda  r:r.choice([True, False]), set_no_jiggle)
+    res.modifier_dict['no_jiggle'] = pm(False, lambda  r:r.choice([True]), set_no_jiggle)
     res.modifier_dict['bfgs_for_rough'] = pm(False, lambda r:r.choice([True]), set_rough_bfgs)
-    res.modifier_dict['scale_rough_iterations'] = pm(1., lambda r:r.uniform(0.3,0.6), scale_rough_iterations)
-    res.modifier_dict['scale_fine_iterations'] = pm(1., lambda r:r.uniform(0.2,0.3), scale_fine_iterations)
+    res.modifier_dict['scale_rough_iterations'] = pm(1., lambda r:0.3, scale_rough_iterations)
+    res.modifier_dict['scale_fine_iterations'] = pm(1., lambda r:0.3, scale_fine_iterations)
     res.modifier_dict['rough_steps'] = pm(1, lambda r:r.choice([1]), set_number_of_rough_steps)
     res.modifier_dict['fine_steps'] = pm(3, lambda r:r.choice([2]), set_number_of_fine_steps)
-    res.modifier_dict['JiggleClusterSmallMaxN'] = pm(5, lambda r:r.integers(3,20), set_JiggleClusterSmallMaxN)
-    res.modifier_dict['JiggleClusterBigMaxN'] = pm(5, lambda r:r.integers(3,20), set_JiggleClusterBigMaxN)
-    res.modifier_dict['TwistMinRadius'] = pm(0., lambda r:r.uniform(0.,1.), set_TwistMinRadius)
-    res.modifier_dict['TwistMaxRadius'] = pm(0., lambda r:r.uniform(1.,3.), set_TwistMaxRadius)
-    res.modifier_dict['CrossoverMaxNtrees'] = pm(20, lambda r:r.integers(2,20), set_CrossoverMaxNtrees)
-    res.modifier_dict['CrossoverSimpleMate'] = pm(False, lambda r:r.choice([True, False]), set_CrossoverSimpleMate)
-    res.modifier_dict['CrossoverP'] = pm(0.4, lambda r:r.uniform(0.,4.), set_CrossoverP)
+    res.modifier_dict['JiggleClusterSmallMaxN'] = pm(5, lambda r:20, set_JiggleClusterSmallMaxN)
+    res.modifier_dict['JiggleClusterBigMaxN'] = pm(5, lambda r:20, set_JiggleClusterBigMaxN)
+    res.modifier_dict['TwistMinRadius'] = pm(0., lambda r:0.5, set_TwistMinRadius)
+    res.modifier_dict['TwistMaxRadius'] = pm(0., lambda r:2., set_TwistMaxRadius)
+    res.modifier_dict['CrossoverMaxNtrees'] = pm(20, lambda r:20, set_CrossoverMaxNtrees)
+    res.modifier_dict['CrossoverSimpleMate'] = pm(False, lambda r:False, set_CrossoverSimpleMate)
+    res.modifier_dict['CrossoverP'] = pm(0.4, lambda r:3., set_CrossoverP)
     
 
-    # Add modifiers to disable each move with 20% probability
-    # Get move names from base_ga
-    for move_item in res.base_ga.move.moves:
-        move_name = move_item[1]  # Extract move name
-        res.modifier_dict[f'disable_{move_name}'] = pm(
-            False,
-            lambda r: r.uniform() < 0.2,  # 20% chance to disable
-            disable_move
-        )
+    # # Add modifiers to disable each move with 20% probability
+    # # Get move names from base_ga
+    # for move_item in res.base_ga.move.moves:
+    #     move_name = move_item[1]  # Extract move name
+    #     res.modifier_dict[f'disable_{move_name}'] = pm(
+    #         False,
+    #         lambda r: r.uniform() < 0.2,  # 20% chance to disable
+    #         disable_move
+    #     )
 
-    del res.modifier_dict['disable_JiggleTreeSmall']
+    # del res.modifier_dict['disable_JiggleTreeSmall']
 
     runner = res.base_ga
     runner.fine_relaxers[0] = pack_dynamics.OptimizerBFGS()
