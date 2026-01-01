ab41abde3e9c13a6721f20d11112bb9da096bec1
Jeroen Cottaar <199474359+jcottaar@users.noreply.github.com>
2026-01-01 20:47:17 +0000

    Dimers!

--- core/pack_runner.py
+++ core/pack_runner.py
@@ -246,12 +246,12 @@ def baseline_runner(fast_mode=False):
 
     res.base_ga = runner
 
-    res.modifier_dict['mate_distance'] = pm(6, lambda r:r.choice([2,4,6]).item(), set_ga_prop)
-    res.modifier_dict['allow_reset_ratio'] = pm(0.5, lambda r:r.uniform(0.5,1.), set_ga_prop)
-    res.modifier_dict['reset_check_generations'] = pm(50, lambda r:r.choice([50,100,200]).item(), set_ga_base_ga_prop)
-    res.modifier_dict['diversity_reset_threshold'] = pm(5./40, lambda r:r.choice([5./40, -1.]).item(), set_ga_prop)
-    res.modifier_dict['N'] = pm(16, lambda r:r.choice([8,16,32]).item(), set_ga_prop)
-    res.modifier_dict['actually_use_champions'] = pm(False, lambda r:r.choice([False,True]).item(), set_ga_prop)
+    res.modifier_dict['mate_distance'] = pm(6, lambda r:r.choice([2,4,6]).item(), set_ga_prop) # 6
+    res.modifier_dict['allow_reset_ratio'] = pm(0.5, lambda r:r.uniform(0.5,1.), set_ga_prop) # 0.95
+    res.modifier_dict['reset_check_generations'] = pm(50, lambda r:r.choice([50,100,200]).item(), set_ga_base_ga_prop) # 100
+    res.modifier_dict['diversity_reset_threshold'] = pm(5./40, lambda r:r.choice([5./40, -1.]).item(), set_ga_prop) # -1.
+    res.modifier_dict['N'] = pm(16, lambda r:r.choice([8,16,32]).item(), set_ga_prop) # 32
+    res.modifier_dict['actually_use_champions'] = pm(False, lambda r:True, set_ga_prop) # True
 
     
     #res.modifier_dict['make_single'] = pm(False, lambda r:False, make_single)
